b3674d72b1440dbc6f3236d7668c5355329e7b23
noobvishal <48903549+noobvishal@users.noreply.github.com>
2019-05-29 01:23:05 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -50,6 +50,8 @@ if CONFIG_CHECK:
 
 API_KEY = os.environ.get("API_KEY", None)
 
+OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
+
 API_HASH = os.environ.get("API_HASH", None)
 
 BOTLOG_CHATID = int(os.environ.get("BOTLOG_CHATID", "0"))
