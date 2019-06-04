5cddbeba1bfe4c0cc5248f7b835e9258146add92
noobvishal <48903549+noobvishal@users.noreply.github.com>
2019-06-05 00:14:09 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -13,19 +13,20 @@ from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
 from userbot import LOGS, bot
 from userbot.modules import ALL_MODULES
 
-DB = connect("")
-CURSOR = DB.cursor()
-CURSOR.execute("""SELECT * FROM BRAIN1""")
-ALL_ROWS = CURSOR.fetchall()
 INVALID_PH = '\nERROR: The Phone No. entered is INVALID' \
              '\n  Tip: Use Country Code along with No.' \
              '\n       Recheck your Phone Number'
+try:
+    bot.start()
+except PhoneNumberInvalidError:
+    print(INVALID_PH)
+    exit(1)
 
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
 LOGS.info("Your Bot is alive! Test it by typing .alive on any chat."
-          " Should you need assistance, head to https://t.me/noobvishal or https://t.me/userbot_support")
+          " Should you need assistance, head to https://t.me/userbot_support")
 LOGS.info("Your Bot Version is 2.4.2")
 
 if len(argv) not in (1, 3, 4):
